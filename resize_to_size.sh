e56e90e6fcc976d290aae2c150a3161537945f71
Dmytro <popovfreewind@gmail.com>
2024-06-25 18:45:39 +0200

    chore: Add output directory creation to resize_to_size.sh script

--- resize_to_size.sh
+++ resize_to_size.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# Create the output directory if it doesn't exist
+mkdir -p output
+
 # Loop through all image files in the 'execute' directory
 find execute \( -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" \) -print0 | while IFS= read -r -d $'\0' initialPath; do
     # Extract the file name without the extension
     filename=$(basename -- "$initialPath")
     filename_no_ext="${filename%.*}"
-    # Extract the directory name
-    dirname=$(dirname -- "$initialPath")
     
-    # Create a new path with the .png extension
-    newPath="$dirname/${filename_no_ext}.png"
+    # Create a new path in the output directory with the .png extension
+    newPath="output/${filename_no_ext}.png"
 
     # Resize the image to 650x650 pixels using ImageMagick
     magick "$initialPath" -resize 650x650\! "$newPath"
